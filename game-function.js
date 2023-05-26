13008b1ef21ceec0b1d7e0b892cf2d3b29ae94d7
ChrisClevenger <aenguscoen@gmail.com>
2023-05-26 16:48:54 -0500

    Create function for random computerSelection in js

--- game-function.js
+++ game-function.js
@@ -1,7 +1,22 @@
 /* This is the first iteration of the game. It will be played completely from the console. */
 /* The first function will getComputerChoice and will return either "Rock", "Paper", or "Scissors".*/
 console.log("Hello world!");
-/*Equate the result of getComptuerChoice with computerSelection*/
+
+const choices = ["rock", "paper", "scissors"];
+
+function computerSelection(choices) {
+
+return choices[Math.floor(Math.random()*choices.length)];
+}
+
+console.log(computerSelection(choices));
 
 /* Prompt user for their selection: rock, paper, or scissors */
 
+/* Create series of if/else functions that determine win/loss scenarios. */
+
+/* Make the users input case insensitive */
+
+/* Console log the results playRound */
+
+/* Loop the game so as to create a 'best of five' game cycle using a function called game()*/
