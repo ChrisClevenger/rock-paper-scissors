fae6733ce165bb304e5c8a2ae794600bf0b35fe4
ChrisClevenger <aenguscoen@gmail.com>
2023-05-30 17:42:01 -0500

    Create js function to log results in scoreboard

--- game-function.js
+++ game-function.js
@@ -2,7 +2,6 @@ const choices = ["rock", "paper", "scissors"];
 
 function computerSelection(choices) {
   return choices[Math.floor(Math.random() * choices.length)];
-  console.log("The computer has chosen!"); 
 }
 
 const win = "You WIN this round!";
@@ -12,6 +11,7 @@ const lose = "You LOSE this round!";
 function playRound(computerSelection, userSelection) {
   if (userSelection === computerSelection) {
     console.log(tie);
+    scoreboard.appendChild(tieLog); 
     return "tie";
   } else if (
     (userSelection === "rock" && computerSelection === "scissors") ||
@@ -19,9 +19,11 @@ function playRound(computerSelection, userSelection) {
     (userSelection === "scissors" && computerSelection === "paper")
   ) {
     console.log(win);
+    scoreboard.appendChild(winLog); 
     return "win";
   } else {
     console.log(lose);
+    scoreboard.appendChild(lossLog); 
     return "lose";
   }
 }
@@ -32,13 +34,38 @@ const scissorsButton = document.querySelector("#scissorsButton");
 
 rockButton.addEventListener("click", () => {
   console.log("User has chosen ROCK!"); 
-  return userSelection = "rock";
+  const userSelection = "rock";
+  const computerChoice = computerSelection(choices);
+  console.log(`The CPU has chosen ${computerChoice}!`); 
+  playRound(computerChoice, userSelection);
 })
 
 paperButton.addEventListener("click", () => {
-  return userSelection = "paper";  
+  console.log("User has chosen PAPER!"); 
+  const userSelection = "paper";
+  const computerChoice = computerSelection(choices);
+  console.log(`The CPU has chosen ${computerChoice}!`); 
+  playRound(computerChoice, userSelection);
 })
 
 scissorsButton.addEventListener("click", () => {
-  return userSelection = "scissors";
+  console.log("User has chosen SCISSORS!"); 
+  const userSelection = "scissors";
+  const computerChoice = computerSelection(choices);
+  console.log(`The CPU has chosen ${computerChoice}!`); 
+  playRound(computerChoice, userSelection);
 })
+
+const scoreboard = document.querySelector(".results"); 
+
+const winLog = document.createElement('p');
+winLog.style.cssText = "color: red;"; 
+winLog.textContent = "You fucking won this round!"; 
+
+const lossLog = document.createElement('p');
+lossLog.style.cssText = "color: red;"; 
+lossLog.textContent = "Doofus! You lost this round!"; 
+
+const tieLog = document.createElement('p');
+tieLog.style.cssText = "color: red;"; 
+tieLog.textContent = "Evenly matched! This round is a tie!"; 
