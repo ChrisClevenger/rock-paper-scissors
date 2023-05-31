95d88d132d260cd93206942f504c0842fe97cb15
ChrisClevenger <aenguscoen@gmail.com>
2023-05-30 20:17:55 -0500

    Write js function to run best of 3 and reset game Created a function to checkGameResults that is performed after each round. The function announces winner when either user or computer reaches 3 wins. Automatically resets and begins again.
    Next functions should clear out the .results div class and create a
    tracker for wins/losses to be displayed on the page.

--- game-function.js
+++ game-function.js
@@ -1,4 +1,6 @@
 const choices = ["rock", "paper", "scissors"];
+let userWins = 0; 
+let computerWins = 0; 
 
 function computerSelection(choices) {
   return choices[Math.floor(Math.random() * choices.length)];
@@ -9,9 +11,23 @@ const tie = "This round is a TIE!";
 const lose = "You LOSE this round!";
 
 function playRound(computerSelection, userSelection) {
+
+  const winLog = document.createElement('p');
+  winLog.style.cssText = "color: red;"; 
+  winLog.textContent = "You fucking won this round!"; 
+
+  const lossLog = document.createElement('p');
+  lossLog.style.cssText = "color: red;"; 
+  lossLog.textContent = "Doofus! You lost this round!"; 
+
+  const tieLog = document.createElement('p');
+  tieLog.style.cssText = "color: red;"; 
+  tieLog.textContent = "Evenly matched! This round is a tie!"; 
+
   if (userSelection === computerSelection) {
     console.log(tie);
     scoreboard.appendChild(tieLog); 
+    checkGameResult(); 
     return "tie";
   } else if (
     (userSelection === "rock" && computerSelection === "scissors") ||
@@ -19,18 +35,41 @@ function playRound(computerSelection, userSelection) {
     (userSelection === "scissors" && computerSelection === "paper")
   ) {
     console.log(win);
-    scoreboard.appendChild(winLog); 
+    scoreboard.appendChild(winLog);
+    userWins++;
+    checkGameResult(); 
     return "win";
   } else {
     console.log(lose);
-    scoreboard.appendChild(lossLog); 
+    scoreboard.appendChild(lossLog);
+    computerWins++; 
+    checkGameResult(); 
     return "lose";
   }
+}
+
+function checkGameResult() {
+  if (userWins === 3) {
+    console.log("You won the game!");
+    resetGame();
+  } else if (computerWins === 3) {
+    console.log("You lost the game!");
+    resetGame();
+  }
+}
+
+function resetGame() {
+  userWins = 0;
+  computerWins = 0;
 }
 
 const rockButton = document.querySelector("#rockButton");
 const paperButton = document.querySelector("#paperButton"); 
 const scissorsButton = document.querySelector("#scissorsButton"); 
+
+const scoreboard = document.querySelector(".results"); 
+const bestOf = document.querySelector(".bestOf"); 
+
 
 rockButton.addEventListener("click", () => {
   console.log("User has chosen ROCK!"); 
@@ -56,16 +95,4 @@ scissorsButton.addEventListener("click", () => {
   playRound(computerChoice, userSelection);
 })
 
-const scoreboard = document.querySelector(".results"); 
 
-const winLog = document.createElement('p');
-winLog.style.cssText = "color: red;"; 
-winLog.textContent = "You fucking won this round!"; 
-
-const lossLog = document.createElement('p');
-lossLog.style.cssText = "color: red;"; 
-lossLog.textContent = "Doofus! You lost this round!"; 
-
-const tieLog = document.createElement('p');
-tieLog.style.cssText = "color: red;"; 
-tieLog.textContent = "Evenly matched! This round is a tie!"; 
